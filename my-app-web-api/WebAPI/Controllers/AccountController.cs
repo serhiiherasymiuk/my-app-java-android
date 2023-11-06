172e4c64fb620aca25777bdc369ed9eaf1247530
Serhii Herasymiuk <sgerasimuk07@gmail.com>
2023-11-06 19:38:31 +0200

    Added JwtService Login

--- my-app-web-api/WebAPI/Controllers/AccountController.cs
+++ my-app-web-api/WebAPI/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using WebAPI.Constants;
 using WebAPI.Data.Entities.Identity;
 using WebAPI.Helpers;
@@ -13,9 +14,13 @@ namespace WebAPI.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<UserEntity> _userManager;
-        public AccountController(UserManager<UserEntity> userManager)
+        private readonly SignInManager<UserEntity> _signInManager;
+        private readonly JwtService _jwtService;
+        public AccountController(UserManager<UserEntity> userManager, SignInManager<UserEntity> signInManager, JwtService jwtService)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtService = jwtService;
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model)
@@ -46,6 +51,19 @@ namespace WebAPI.Controllers
             {
                 return BadRequest();
             }
+        }
+
+        [HttpPost("login")]
+        public async Task<LoginResponseViewModel> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            await _signInManager.SignInAsync(user, true);
+
+            return new LoginResponseViewModel()
+            {
+                Token = _jwtService.CreateToken(_jwtService.GetClaims(user))
+            };
         }
     }
 }
